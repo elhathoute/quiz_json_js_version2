edb56613e182438d74d41aae10b63542ea411210
elhathoute <abdelaazizeelhathoute.2018@gmail.com>
2023-01-02 23:52:41 +0100

    make changes in db

--- ModalQuiz.php
+++ ModalQuiz.php
@@ -6,8 +6,17 @@ require_once 'db.php';
        
      
         public function getQuestions(){
-        $sql = "SELECT a.id as 'id_reponse',q.nom_question as 'question',a.option_0,a.option_1,a.option_2,a.option_3,a.explication FROM questions q
-         inner join answers a on a.question_id=q.id ";
+        $sql = "
+        
+        select DISTINCT  q.question as question,
+        (select a.nom_answer from answers as a where a.order like 1 AND a.question_id like q.id ) as option_0,
+         (select a.nom_answer from answers as a where a.order like 2 AND a.question_id like q.id ) as option_1,         (select a.nom_answer from answers as a where a.order like 3 AND a.question_id like q.id ) as option_2,         (select a.nom_answer from answers as a where a.order like 4 AND a.question_id like q.id ) as option_3,
+             (select a.explication from answers as a where a.etat=1 AND a.question_id like q.id ) as explication,
+              (select a.id from answers as a where a.order like 1 AND a.question_id like q.id ) as id_0,
+         (select a.id from answers as a where a.order like 2 AND a.question_id like q.id ) as id_1,         (select a.id from answers as a where a.order like 3 AND a.question_id like q.id ) as id_2,         (select a.id from answers as a where a.order like 4 AND a.question_id like q.id ) as id_3
+         from answers as a, questions q
+        where q.id  = a.question_id 
+        ";
         $stm = $this->connexion()->query($sql);
         $result = $stm->fetchAll();
         return $result;
