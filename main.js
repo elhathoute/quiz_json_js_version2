9b58d4169249e076fca5b8e4f55cd3535ce397dc
abdelaziz <abdelaazizeelhathoute.2018@gmail.com>
2023-01-04 14:42:41 +0100

    some modifications

--- main.js
+++ main.js
@@ -1,90 +1,19 @@
+$(document).ready(function () {
+  $.get('ControllerQuiz.php', function(data) {
+    $.get('ModalQuiz.php', function(data) {
+
+      });
+  });
+
+ 
+
 fetch('quiz.json')
   .then(response => response.json())
   .then(array => {
 
      var questions=array;
     console.log(questions);
-
-
-
-
-/*
-let questions =[
-  {
-    
-   question:"1-Why is AWS more economical than traditional data centers for applications with varying compute  workloads?",
-      0:"Amazon EC2 costs are billed on a monthly basi",
-      1:"Users retain full administrative access to their Amazon EC2 instance",
-      2:"Amazon EC2 instances can be launched on demand when needed",
-      3:"Users can permanently run enough instances to handle peak workloads.",
-      vrai:"Amazon EC2 instances can be launched on demand when needed",
-      explication:"Q1:C-The ability to <a href='https://docs.aws.amazon.com/whitepapers/latest/aws-overview/six-advantages-of-cloud-computing.html'> launch instances on demand </a> when needed allows users to launch and terminate instances in response to a varying workload. This is a more economical practice than purchasing enough on-premises servers to handle the peak load."
-      
-  },{
-    question:"2-Which AWS service would simplify the migration of a database to AWS?",
-    0:"AWS Storage Gateway",
-    1:"AWS Database Migration Service (AWS DMS)",
-    2:"Amazon EC2",
-    3:"Users can permanently run enough instances to handle peak workloads.",
-    vrai:"AWS Database Migration Service (AWS DMS)",
-    explication:"Q2:B- AWS DMS helps users migrate databases to AWS quickly and securely. The source database remains fully operational during the migration, minimizing downtime to applications that rely on the database.<a href='https://aws.amazon.com/fr/dms/'> AWS DMS</a> can migrate data to and from most widely used commercial and open-source databases."
-    
-  },{
-    question:"3-Which AWS offering enables users to find, buy, and immediately start using software solutions in their AWS environment?",
-    0:"AWS Config",
-    1:"AWS OpsWorks",
-    2:"Amazon  AWS SDK",
-    3:"AWS Marketplace",
-    vrai:"AWS Marketplace",
-    explication:"Q3:D- <a href='https://aws.amazon.com/marketplace/'>AWS Marketplace</a> is a digital catalog with thousands of software listings from independent software vendors that makes it easy to find, test, buy, and deploy software that runs on AWS."
-    
-  },{
-    question:"4-Which AWS networking service enables a company to create a virtual network within AWS?",
-    0:"AWS Config",
-    1:"Amazon Route 53",
-    2:"AWS Direct Connect",
-    3:"Amazon Virtual Private Cloud (Amazon VPC)",
-    vrai:"Amazon Virtual Private Cloud (Amazon VPC)",
-    explication:"Q4:D- <a href='https://aws.amazon.com/vpc/'>Amazon VPC<a> lets users provision a logically isolated section of the AWS Cloud where users can launch AWS resources in a virtual network that they define."
-    
-  },{
-    question:"5-Which of the following is an AWS responsibility under the AWS shared responsibility model?",
-    0:"Configuring third-party applications",
-    1:"Maintaining physical hardware",
-    2:"Securing application access and data",
-    3:"Managing guest operating systems",
-    vrai:"Maintaining physical hardware",
-    explication:"Q5:B- – Maintaining physical hardware is an AWS responsibility under the<a href='https://aws.amazon.com/compliance/shared-responsibility-model/'> AWS shared responsibility model<a> "
-    
-  },{
-    question:"6- Which component of the AWS global infrastructure does Amazon CloudFront use to ensure low-latency delivery?",
-    0:"AWS Regions",
-    1:"Edge locations",
-    2:"Availability Zones",
-    3:"Virtual Private Cloud (VPC)",
-    vrai:"Edge locations",
-    explication:"Q6:B- – To deliver content to users with lower latency<a href='https://aws.amazon.com/cloudfront/'>Amazon CloudFront<a> uses a global network of points of presence (edge locations and regional edge caches) worldwide "
-  }
-  ,{
-    question:"7-  How would a system administrator add an additional layer of login security to a user's AWS Management Console?",
-    0:"Use Amazon Cloud Directory",
-    1:"Audit AWS Identity and Access Management (IAM) roles",
-    2:"Enable multi-factor authentication",
-    3:"Enable AWS CloudTrail",
-    vrai:"Enable multi-factor authentication",
-    explication:"Q7:C- – <a href='https://aws.amazon.com/iam/features/mfa/'>Multi-factor authentication<a>  (MFA) is a simple best practice that adds an extra layer of protection on top of a  username and password. With MFA enabled, when a user signs in to an AWS Management Console, they will be prompted for their username and password (the first factor—what they know), as well as for an authentication code from their MFA device (the second factor—what they have). Taken together, these multiple factors provide  increased security for AWS account settings and resources "
-  } ,{
-    question:"8-  Which service can identify the user that made the API call when an Amazon EC2 instance is terminated?",
-    0:"AWS Trusted Advisor",
-    1:"AWS CloudTrail",
-    2:"AWS X-Ray",
-    3:"AWS Identity and Access Management (AWS IAM)",
-    vrai:"AWS CloudTrail",
-    explication:"Q8:B- – <a href='https://docs.aws.amazon.com/awscloudtrail/latest/userguide/cloudtrail-user-guide.html'> AWS CloudTrail<a>  helps users enable governance, compliance, and operational and risk auditing of their AWS accounts. Actions taken by a user, role, or an AWS service are recorded as events in CloudTrail. Events  include actions taken in the AWS Management Console, AWS Command Line Interface (CLI), and AWS SDKs and APIs. "
-  } 
-
-];
-*/
+//random array
 function randomOfArray(array) {
   var j, x, i;
   for (i = array.length - 1; i > 0; i--) {
@@ -164,7 +93,7 @@ next.addEventListener('click',function(){
  
 
   //time count down
-  countDownTime(30,questionCount);
+  countDownTime(10,questionCount);
   //create a div to store name of user
   const divUser = document.createElement("div");
   user_input.replaceWith(divUser);
@@ -192,7 +121,7 @@ next.addEventListener('click',function(){
     
 
   let lesReponse=document.querySelectorAll("input[type=radio]");
-  console.log(lesReponse);
+  // console.log(lesReponse);
   lenghtQuestion=lesReponse.length;
 for(i=0;i<lenghtQuestion;i++){
   
@@ -214,7 +143,7 @@ for(i=0;i<lenghtQuestion;i++){
    // Use clearInterval() to stop time
   clearInterval(countDownTimeInterval);
   //add coun down of 30 seconds
-  countDownTime(30,questionCount);
+  countDownTime(10,questionCount);
  
      //get the right answer
      let reponsrvraix=questions[currentIndex].vrai;
@@ -370,7 +299,7 @@ function compareReponse(answer,index){//current index
         //   console.log(random[i]);
         // }
        
-        console.log(arrayToSentInDb);
+        // console.log(arrayToSentInDb);
 
         // if(answer==ReponseChoisi){
         //   // arrayCorrectQuest.push(currentIndex-1);   
@@ -380,6 +309,9 @@ function compareReponse(answer,index){//current index
         //   console.log('nonegaux'); 
         //   console.log(arrayToSentInDb);
         // }
+  }else{
+    arrayToSentInDb.push({'id_rep_choisi':0,'indexRandom':currentIndex-1});
+
   }
   
 
@@ -399,15 +331,15 @@ function afficherResultat(count,array){
      if ((arrayCorrectQuest.length)==count){ 
 
       
-      answers.innerHTML = '<span id="nom-user">'+nomUser +'</span> congratulation! 🎉, You got '+ (arrayCorrectQuest.length) +'</p> out of <p>'+ count +'</p></span>';
+      answers.innerHTML = '<span id="nom-user">'+nomUser +'</span> congratulation! 🎉, You got  '+ (arrayCorrectQuest.length) +'</p> out of <p>'+ count +'</p></span>';
 
   }
   else if((arrayCorrectQuest.length) >=(count/2) ){
-    answers.innerHTML= '<span id="nom-user">'+nomUser +'</span>   nice 😎, You got'+ (arrayCorrectQuest.length) +' out of '+ count +'</span>';
+    answers.innerHTML= '<span id="nom-user">'+nomUser +'</span>   nice 😎, You got  '+ (arrayCorrectQuest.length) +' out of '+ count +'</span>';
       
   }
   else { 
-    answers.innerHTML= '<span id="nom-user">'+nomUser +'</span>  sorry 😐, You got only'+ (arrayCorrectQuest.length) +' out of '+ count +'</span>';
+    answers.innerHTML= '<span id="nom-user">'+nomUser +'</span>  sorry 😐, You got only  '+ (arrayCorrectQuest.length) +' out of '+ count +'</span>';
       
   }
    
@@ -456,6 +388,7 @@ function countDownTime(time,count){
     if(--time<0){
       clearInterval(countDownTimeInterval);
       submit.click();
+     
     
     }
        
@@ -468,3 +401,5 @@ function countDownTime(time,count){
 }
     
 );
+
+})
